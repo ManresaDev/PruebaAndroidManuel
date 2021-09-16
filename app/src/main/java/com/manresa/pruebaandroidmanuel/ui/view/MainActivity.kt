2945b4eb8ad4b7e4dc328194fc463b99c22f6876
ManresaDev <ManreDev@gmail.com>
2021-09-16 18:20:03 +0200

    Añadido buscador muy simple y detectar el deslizamiento

--- app/src/main/java/com/manresa/pruebaandroidmanuel/ui/view/MainActivity.kt
+++ app/src/main/java/com/manresa/pruebaandroidmanuel/ui/view/MainActivity.kt
@@ -6,8 +6,10 @@ import android.icu.number.NumberRangeFormatter.with
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,11 +23,12 @@ import com.manresa.pruebaandroidmanuel.utils.ShowAlert
 import com.squareup.picasso.Picasso
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     private lateinit var binding : ActivityMainBinding
     private lateinit var token : String
     private lateinit var device : String
     private val viewmodel : MainViewModel by viewModels()
+    private lateinit var carteleras : ArrayList<Content>
 
     companion object{
         lateinit var adapter : CarteleraAdapter
@@ -38,6 +41,11 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        binding.buscador.setOnQueryTextListener(this)
+
+        carteleras = ArrayList()
+
+
         recyclerViewCartelera = binding.recyclerview
 
         adapter = CarteleraAdapter(this)
@@ -48,7 +56,27 @@ class MainActivity : AppCompatActivity() {
         getToken()
         getDevice()
         getCartelera()
+        setRecycler()
+        deslizamiento()
+
         hide()
+    }
+
+    private fun deslizamiento() {
+        recyclerViewCartelera.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if(dy >0 && binding.buscador.visibility == View.VISIBLE){
+                    binding.buscador.visibility = View.GONE
+                }else if(dy<0 && binding.buscador.visibility != View.VISIBLE){
+                    binding.buscador.visibility = View.VISIBLE
+                }
+            }
+        })
+    }
+
+    private fun setRecycler() {
+        adapter.setListData(carteleras)
     }
 
     private fun getDevice() {
@@ -61,7 +89,8 @@ class MainActivity : AppCompatActivity() {
           binding.titulo.text = it.user.name
           Picasso.with(this).load(it.user.avatar).into(binding.avatar);
         binding.loading.visibility = View.GONE
-          adapter.setListData(it.contents as MutableList<Content>)
+          //adapter.setListData(it.contents as MutableList<Content>)
+          carteleras.addAll(it.contents)
           adapter.notifyDataSetChanged()
       })
     }
@@ -84,6 +113,35 @@ class MainActivity : AppCompatActivity() {
 
     private fun hide() {
         supportActionBar?.hide()
+    }
+
+    private fun searchByName(query : String){
+        viewmodel.getCartelera(this, token, device).observe(this, Observer {
+            for (result in it.contents){
+                if(result.title == query){
+
+                    carteleras.clear()
+                    carteleras.addAll(listOf(result))
+                    adapter.notifyDataSetChanged()
+                }
+            }
+            hideKeyBoard()
+        })
+    }
+    private fun hideKeyBoard() {
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.viewroot.windowToken, 0)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        if(!query.isNullOrEmpty()){
+            searchByName(query)
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
     }
 
 
